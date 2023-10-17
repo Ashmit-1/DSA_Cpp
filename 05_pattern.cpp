af4cd838814b87a3bbd14673d515a0c680c1c580
Ashmit Das <137298461+Ashmit-1@users.noreply.github.com>
2023-10-17 10:04:31 +0000

    More problems on patterns done

---
+++ 05_pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,rows;
+    cout<<"Enter the number of rows: ";
+    cin>>rows;
+    for(i=1;i<=rows;i++){
+        for(j=i;j>0;j--){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
